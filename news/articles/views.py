6c7c73d2ebda3dfc5574ac244168a80332cf4cd9
Saeid Gholami <saeidxgholami@gmail.com>
2022-02-01 13:45:13 +0330

    Autherization

--- news/articles/views.py
+++ news/articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView, ListView, CreateView
 from django.views.generic.edit import DeleteView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Article
 
@@ -20,20 +21,32 @@ class ArticleDetailView(DetailView):
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ("title", "body")
     template_name = "article_update.html"
 
+    def test_func(self):
+        current_article = self.get_object()
+        return current_article.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
 
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
+    def test_func(self):
+        current_article = self.get_object()
+        return current_article.author == self.request.user
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_create.html"
-    fields = ("title", "body", "author")
+    fields = ("title", "body")
+    login_url = "login"
+
+    def form_valid(self, form):
+        form.instance = self.request.user
+        super().form_valid(form)
